83344daf739bde7c889b69863d6a1f8526c5e5e0
Tulloch022 <90489018+tulloch022@users.noreply.github.com>
2022-03-12 22:34:20 -0800

    added script to subtract from guesses counter with each box clicked

--- app.js
+++ app.js
@@ -8,22 +8,28 @@ const mineLocation = (Math.floor(Math.random() * 56))
 counter = 0;
 const clickBox = () => {
     console.log(counter)
-    if (counter < 3) {
+    if (counter < 15) {
         window.onclick = e => {
             let clickedBox = e.target;
             const style = getComputedStyle(clickedBox);
             const backgroundColor = style.backgroundColor;
              if (backgroundColor == "rgb(211, 222, 213)") {
                  clickedBox.style.backgroundColor = "black";
-                 counter+=1;        
-            } else {
-            clickedBox.style.backgroundColor = "rgb(211, 222, 213)";
-            counter-=1;
+                 counter+=1;
+                 guessTaker()        
              }
          }
     } else { 
-        alert ('You lost!');
+        alert ('You ran out of guesses!');
         window.location.reload()}
         
 };
+
+
+//Update selected counter
+
+
+const guessTaker = () => {
+    document.querySelector("#guesses").innerText-=1;
+}
 
