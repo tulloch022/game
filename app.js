e6cbc493d9fe364681e676db801df64e2a69ca80
Tulloch022 <90489018+tulloch022@users.noreply.github.com>
2022-03-12 22:08:57 -0800

    if 4 boxes are selected alert and refresh

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
-//Update Counter
+//Plant cat randomly
+
+const mineLocation = (Math.floor(Math.random() * 56))
 
 
 //Event: Clicked box
 
+counter = 0;
 const clickBox = () => {
-    window.onclick = e => {
-        let clickedBox = e.target;
-        const style = getComputedStyle(clickedBox);
-        const backgroundColor = style.backgroundColor;
-        if (backgroundColor == "rgb(211, 222, 213)") {
-            clickedBox.style.backgroundColor = "black";        
-        } else {
+    console.log(counter)
+    if (counter < 3) {
+        window.onclick = e => {
+            let clickedBox = e.target;
+            const style = getComputedStyle(clickedBox);
+            const backgroundColor = style.backgroundColor;
+             if (backgroundColor == "rgb(211, 222, 213)") {
+                 clickedBox.style.backgroundColor = "black";
+                 counter+=1;        
+            } else {
             clickedBox.style.backgroundColor = "rgb(211, 222, 213)";
-        }
-    }
+            counter-=1;
+             }
+         }
+    } else { 
+        alert ('You lost!');
+        window.location.reload()}
+        
 };
 
